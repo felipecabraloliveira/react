86a1267a41c9da84bbaccf9b80385e23d2e827e5
Felipe Cabral <felipecabral33@gmail.com>
2022-04-30 18:28:49 -0300

    Curso de React

--- meu-projeto/src/components/Evento.js
+++ meu-projeto/src/components/Evento.js
@@ -14,13 +14,11 @@ function Evento({ numero }) {
         console.log('Ativando segundo evento!')
     }
 
-
     return (
         <div>
             <p>Clique para disparar um evento.</p>
             <Button event={meuEvento} text="Primeiro Evento" />
             <Button event={segundoEvento} text="Segundo Evento" />
-
         </div>
     )
 }
